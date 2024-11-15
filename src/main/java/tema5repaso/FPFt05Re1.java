cc57ccc9a5e731e12b9fb53ca09feb2c4934847e
fernando.pargafernan <fernando.pargafernan@027P134C.wirtz.local>
2024-11-15 19:04:11 +0100

    Crear

---
+++ src/main/java/tema5repaso/FPFt05Re1.java
@@ -0,0 +1,14 @@
+package tema5repaso;
+
+import java.util.Scanner;
+
+public class FPFt05Re1 {
+    
+    /*
+    
+    */
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
