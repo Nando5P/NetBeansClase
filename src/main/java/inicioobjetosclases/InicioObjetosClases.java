706b57a86f7c86b2bb28cecb67a134859611456c
50nan <50nan@Nando5P>
2024-11-20 15:32:43 +0100

    create

---
+++ src/main/java/inicioobjetosclases/InicioObjetosClases.java
@@ -0,0 +1,71 @@
+package inicioobjetosclases;
+
+import java.util.Scanner;
+
+/*
+ Crear una clase Libro que contenga los siguientes atributos privados:
+● ISBN
+● Titulo
+● Autor
+● Número de páginas
+● Contador (de tipo public y static) -> atributo de clase
+
+Se implantaran varios constructores:
+○ Un constructor por defecto.
+○ Un constructor con el Titulo, el autor y el resto por defecto.
+○ Un constructor con todos los atributos como parámetro.
+
+El atributo contador se incrementa cada vez que se crea un nuevo objeto de la clase Libro.
+Crear sus respectivos métodos get y set correspondientes para cada atributo. 
+Crear el método toString() para mostrar la información relativa al libro con el siguiente formato:
+«El libro con ISBN creado por el autor tiene páginas»
+En el fichero main, crear 2 objetos Libro (los valores que se quieran) y mostrarlos por
+pantalla.
+Imprimir por pantalla el valor del atributo contador sin necesidad de usar una instancia de la
+clase Libro.
+Por último, indicar cuál de los 2 tiene más páginas.
+
+ */
+public class InicioObjetosClases {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        Libro libro1 = new Libro();
+        Libro libro2 = new Libro();
+
+        System.out.println("Introduce titulo del libro1: ");
+        libro1.setTitulo(teclado.nextLine());
+        System.out.println("Introduce ISBN del libro1: ");
+        libro1.setISBN(teclado.nextLine());
+        System.out.println("Introduce autor del libro1: ");
+        libro1.setAutor(teclado.nextLine());
+        System.out.println("Introduce numero de paginas del libro1: ");
+        libro1.setNumPaginas(teclado.nextInt());
+
+        System.out.println("Introduce titulo del libro2: ");
+        libro2.setTitulo(teclado.nextLine());
+        System.out.println("Introduce ISBN del libro2: ");
+        libro2.setISBN(teclado.nextLine());
+        System.out.println("Introduce autor del libro2: ");
+        libro2.setAutor(teclado.nextLine());
+        System.out.println("Introduce numero de paginas del libro2: ");
+        libro2.setNumPaginas(teclado.nextInt());
+
+        System.out.println("Total de libros guardados: " + Libro.contadorLibros);
+        
+        System.out.println(libro1.toString());
+        System.out.println(libro2.toString());
+
+        System.out.println("Numero de Libros creados: " + Libro.contadorLibros);
+
+        if (libro1.getNumPaginas() > libro2.getNumPaginas()) {
+            System.out.println("El libro con mas paginas es: " + libro1.getTitulo());
+        } else {
+            if (libro2.getNumPaginas() > libro1.getNumPaginas()) {
+                System.out.println("El libro con mas paginas es: " + libro2.getTitulo());
+            } else {
+                System.out.println("Los dos libros tienen el mismo numero de paginas.");
+            }
+        }
+    }
+}
---
+++ src/main/java/inicioobjetosclases/Libro.java
@@ -0,0 +1,65 @@
+package inicioobjetosclases;
+
+public class Libro {
+
+    private String ISBN;
+    private String titulo;
+    private String autor;
+    private int numPaginas;
+    public static int contadorLibros = 0;
+
+    public Libro() {
+        contadorLibros++;
+    }
+
+    public Libro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        contadorLibros++;
+    }
+
+    public Libro(String ISBN, String titulo, String autor, int numPaginas) {
+        this.ISBN = ISBN;
+        this.autor = autor;
+        this.titulo = titulo;
+        this.numPaginas = numPaginas;
+        contadorLibros++;
+    }
+
+    public String getISBN() {
+        return this.ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(int numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    public String toString() {
+        return "El libro cpn ISBN " + ISBN + " creado por " + autor + " y de nombre " + titulo + " tiene " + numPaginas + "paginas";
+    }
+
+}
