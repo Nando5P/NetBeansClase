30b7efe232d0de48e1c00f3105d567325d2ca3e1
fernando.pargafernan <fernando.pargafernan@027P134C.wirtz.local>
2024-11-25 20:17:36 +0100

    Crear

--- src/main/java/examen23a24/Ejercicio3.java
+++ src/main/java/examen23a24/Ejercicio3.java
@@ -10,7 +10,11 @@ public class Ejercicio3 {
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
+        Prenda prenda1 = new Prenda("pantalon", 38, 'M', 50.0, 21.0);
+        Prenda prenda2 = new Prenda("camisa", 30.0);
         
+        System.out.println(prenda1.toString());
+        System.out.println(prenda2.toString());
 
     }
 }
--- src/main/java/examen23a24/Prenda.java
+++ src/main/java/examen23a24/Prenda.java
@@ -2,15 +2,14 @@ package examen23a24;
 
 import java.util.Random;
 
-
 /**
  *
  * @author fernando.pargafernan
  */
 public class Prenda {
-    
+
     // Atributos
-    private static long stockPrendas =0;
+    private static long stockPrendas = 0;
     private static String tipo;
     private static int referencia;
     private static double precioConIva;
@@ -18,24 +17,24 @@ public class Prenda {
     private static double iva;
     private static double talla;
     private static char sexo;
-  
+
     // Constantes
     public static final char hombre = 'H';
     public static final char mujer = 'M';
     public static final String falda = "Falda";
     public static final String pantalon = "Pantalon";
     public static final String jersey = "Jersey";
-    
-    public static int generarReferencia(){
+
+    public static int generarReferencia() {
         Random random = new Random();
-    int numeroRandom = random.nextInt(99999)+10000;
-     return numeroRandom;
+        int numeroRandom = random.nextInt(99999) + 10000;
+        return numeroRandom;
     }
-    
-    private void setPrecioConIva(){
-    this.precioConIva = this.precioSinIva + (this.precioSinIva * this.iva / 100);
+
+    private void setPrecioConIva() {
+        this.precioConIva = this.precioSinIva + (this.precioSinIva * this.iva / 100);
     }
-    
+
     public Prenda(String tipo, int talla, char sexo, double precioSinIva, double iva) {
         this.referencia = generarReferencia();
         this.setTipo(tipo);
@@ -45,34 +44,30 @@ public class Prenda {
         stockPrendas++;
         this.setPrecioConIva();
     }
-    
-    public Prenda (String tipo, double precioSinIva){
-    this(tipo, 42, 'H', precioSinIva, 21.0);
+
+    public Prenda(String tipo, double precioSinIva) {
+        this(tipo, 42, 'H', precioSinIva, 21.0);
     }
-    
+
     private void setTipo(String tipo) {
-    if (tipo != null && (tipo.equalsIgnoreCase(pantalon) || tipo.equalsIgnoreCase(jersey) || tipo.equalsIgnoreCase(falda))){
-    this.tipo = tipo.toLowerCase();
-    }else{
-        this.tipo = null;
-    }
-    }
-
-    public String toString(){
-    return "Prenda {"
-            +"tipo='" + tipo + '\''
-            +", talla= " + talla
-            + ", sexo= " + sexo
-            + ", precio sin iva= " + precioSinIva
-            + ", iva= "
+        if (tipo != null && (tipo.equalsIgnoreCase(pantalon) || tipo.equalsIgnoreCase(jersey) || tipo.equalsIgnoreCase(falda))) {
+            this.tipo = tipo.toLowerCase();
+        } else {
+            this.tipo = null;
+        }
     }
 
- 
+    public String toString() {
+        return "Prenda {"
+                + "tipo='" + tipo + '\''
+                + ", talla= " + talla
+                + ", sexo= " + sexo
+                + ", precio sin iva= " + precioSinIva
+                + ", iva= " + iva
+                + ", precio con iva= " + precioConIva
+                + ", Referencia= " + referencia
+                + ", Stock prendas= " + stockPrendas
+                + '}';
+    }
 
-    
-   
-    
-    
-    
-    
 }
