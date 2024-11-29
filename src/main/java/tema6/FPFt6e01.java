4185242b8ca226998c957973549bdf0116d63b82
fernando.pargafernan <fernando.pargafernan@027P134C.wirtz.local>
2024-11-29 18:59:49 +0100

    Crear

---
+++ src/main/java/tema6/FPFt6e01.java
@@ -0,0 +1,34 @@
+package tema6;
+
+import java.util.Scanner;
+
+/**
+ * A partir de la clase MovilPrepago que te proporcionará el profesor, haz un
+ * programa en el que se cree un teléfono con unas características determinadas
+ * y luego ejecute una serie de actividades como recargar 10 euros, llamar 30
+ * segundos, navegar 10MB, etc. Viendo cómo se va modificando el saldo. Las
+ * propiedades de un móvil prepago son: - int numeroMovil (9 dígitos) - float
+ * costeMinutoLlamada (euros, con dos decimales) - float costeConsumoMB (euros,
+ * con dos decimales) - float saldo (euros, con dos decimales) Tiene un único
+ * constructor con la siguiente firma: MovilPrepago(long nM, float cML, float
+ * cMB, float s) Los métodos son: - void efectuarLlamada (int segundos): reduce
+ * el saldo. Si el saldo no es suficiente, se corta la llamada - void
+ * navegar(int MB) análogo a efectuar una llamada - boolean recargar (int
+ * importe): aumenta el saldo, debe ser múltiplo de 5 euros, sino devuelve false
+ * - float consultarSaldo () Lo primero de debemos hacer es crear una instancia
+ * de un móvil (con el constructor) con unos valores cualesquiera, por ejemplo:
+ * MovilPrepago miMovil = new MovilPrepago(900900900L, 0.1f, 0.5f, 10f); y
+ * después llamar a los métodos y mostrar por pantalla el atributo saldo del
+ * móvil.
+ */
+public class FPFt6e01 {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        MovilPrepago movil1 = new MovilPrepago(900900900L, 0.1f, 0.5f, 10f);
+
+        
+        System.out.println("Su telefono es: " + movil1.getSaldo());
+        
+    }
+}
---
+++ src/main/java/tema6/MovilPrepago.java
@@ -0,0 +1,72 @@
+
+package tema6;
+
+public class MovilPrepago {
+    
+    public long numeroMovil;
+    final private float costeMinutoLlamada;
+    final private float costeConsumoMB;
+    private float saldo;
+    
+
+    MovilPrepago(long nM, float cML, float cMB, float s) {
+        numeroMovil = nM;
+        costeMinutoLlamada = cML;
+        costeConsumoMB = cMB;
+        saldo = s;
+    }
+    
+    
+
+    public void efectuarLlamada(int segundos) {
+        saldo -= Math.round((costeMinutoLlamada / 60f * segundos) * 100f) / 100f;
+        if (saldo < 0) {
+            saldo = 0;
+        }
+    }
+
+    public void navegar(int mb) {
+        saldo -= Math.round(costeConsumoMB * mb * 100f) / 100f;
+        if (saldo < 0) {
+            saldo = 0;
+        }
+    }
+
+    public boolean recargar(int importe) {
+        if (importe % 5 == 0) {
+            saldo += importe;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public float consultarSaldo() {
+        return saldo;
+     }
+
+    public long getNumeroMovil() {
+        return numeroMovil;
+    }
+
+    public void setNumeroMovil(long numeroMovil) {
+        this.numeroMovil = numeroMovil;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    
+    
+    @Override
+    public String toString() {
+        return "MovilPrepago{" + "numeroMovil= " + numeroMovil + ", costeMinutoLlamada= " + costeMinutoLlamada + ", costeConsumoMB= " + costeConsumoMB + ", saldo= " + saldo + '}';
+    }
+    
+    
+}
